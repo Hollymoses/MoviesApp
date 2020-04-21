0052faf1a3988d52f28a4f6ba4c1a4cd0bbb5f08
Hollymoses <hollymoses23@gmal.com>
2020-04-21 23:41:06 +0300

    omdb api setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import omdb from "../apis/omdb";
 
-const app = () => {
-    return <div><SearchBar /></div>;
+class app extends React.Component  {
+    onTermSubmit = async term => {
+        const response = await omdb.get('/',{
+          params: {
+            apikey: '8561a479',
+            t: term
+          }
+        });
+    }
+    render() {
+        return (
+            <div><SearchBar onFormSubmit={this.onTermSubmit}/></div>
+        );
+    }
 }
 
 export default app;
